5f0da91097a4ceb129b784a431cf4eb98678549d
Lenisalsabila <lenisalsabila13@gmail.com>
2022-03-11 00:36:48 +0700

    second

---
+++ src/app/baranginduk/baranginduk.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import {Barang} from "../model/barang";
+import {FormBuilder, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-baranginduk',
+  templateUrl: './baranginduk.component.html',
+  styleUrls: ['./baranginduk.component.css']
+})
+export class BarangindukComponent implements OnInit {
+  formAdd! : FormGroup;
+  kirimData! : Barang;
+  constructor(private formBuild: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.formAdd = this.formBuild.group( {
+      'sku' :[null],
+      'qty' : [null],
+      'harga' :[null],
+      'amount' :[null],
+      'total' :[null]
+    });
+  }
+  simpan(): void{
+    let data = <Barang>{};
+    data.sku =this.formAdd.controls['sku'].value
+    data.qty =this.formAdd.controls['qty'].value
+    data.harga =this.formAdd.controls['harga'].value
+    this.kirimData = data;
+  }
+  amount(): number{
+    let jum =<Barang>{};
+    jum.sku =this.formAdd.controls['sku'].value
+    jum.qty =this.formAdd.controls['qty'].value
+    jum.harga =this.formAdd.controls['harga'].value
+    jum.amount =this.formAdd.controls['amount'].value
+    jum.total =this.formAdd.controls['total'].value
+
+      jum.qty * jum.harga;
+
+  }
+
+  terimaPesa($event: any):void{
+    this.formAdd.controls['sku'].setValue($event.sku)
+    this.formAdd.controls['qty'].setValue($event.qty)
+    this.formAdd.controls['harga'].setValue($event.harga)
+
+  }
+
+}
